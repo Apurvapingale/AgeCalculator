8a760b64ef39e6f7041d43a34a1ec2d1c6da8cb5
Apurva <apurvapingale05@gmail.com>
2023-10-14 02:57:17 +0530

    Updated readme

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Title from "./components/Title";
+import Main from "./components/Main";
 
 function App() {
+  const [date, setDate] = useState("");
+  const [age, setAge] = useState(null);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Title />
+      <Main date={date} setDate={setDate} age={age} setAge={setAge} />
     </div>
   );
 }
